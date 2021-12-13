5b33f8bc6661e8eeca9f487d37a41057727e05d9
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-12 21:12:23 -0800

    commit on index, edition. php, edition.css, styles.css and images

---
+++ edition.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>IT162 Final Project - Ruku Svelo</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<script src="https://kit.fontawesome.com/e0101396a3.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/styles.css" />
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.php">rukusvelo</a></h1>
+        <nav>
+            <ul class="topnav">
+                <li><a href="edition.php">#1</a></li>
+                <li><a href="shop.php">Shop</a></li> 
+            </ul>
+        </nav>        
+    </header>
+
+    <section class="pic"> 
+        <img src="images/swim.jpg" style="width:100%;" alt="#1">
+        <img src="images/nasishort.jpg" style="width:100%;" alt="#1">
+    </section>
+    
+    <footer>
+        <p><small>&copy;rukusvelo-<?php echo date("Y")?></p>
+        <nav>
+            <ul class="footernav">
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="size.php">Size</a></li>
+                <li><a href="cart.php">Cart</a></li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+</html>

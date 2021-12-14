8b0cc4e2d24818f8f5e319f0d18a14524cec29cd
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-14 04:14:59 -0800

    removed </br>

--- about.php
+++ about.php
@@ -14,7 +14,7 @@ include 'includes/header.php';
             I want my daughters to lead beautiful lives,</br>
             to enjoy sophisticated aesthetics,</br>
             and to be able to navigate emotional complexith.</br>
-                                                             </br>
+                                                
             I have been dreaming of children's clothes that are</br>
             classic, sturdy, comfortable, simple, wholesome, and honest.</br>
             Ruku Svelo celebrates traditions, the passage of time,</br>
@@ -25,12 +25,12 @@ include 'includes/header.php';
             The birth of Ruku Svelo began<br> with my mother-in-law Ella's wardrobe,<br>
             which is full of linen dresses, wool stockings,<br> hand-printed cottons,
             hand-knitted sweaters,<br> and the old, soft, worn textures of Soviet Russia.<br>
-                                                                    <br>
+                                                                    
             As a designer, I worked for ten years in women's apparel.<br>
             But I quit fast fashion and throw-away culture.<br>
             Now most of my designs are one-offs,<br> made from rare fabrics,<br>
             hand-selected for quality,<br>designed to be passed down through several generations.<br>
-            <br> 
+             
             I live with my husband and two daughters in Maine.<br>
             We are surrounded by water, mountains,<br>
             and evergreen forests.                                                        

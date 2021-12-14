86d26c7481f8022cb0d160bee57e2a90a05f7d4e
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-13 18:22:16 -0800

    changed name 'edition' to 'editionone.php'

---
+++ editionone.php
@@ -0,0 +1,15 @@
+<?php 
+include 'includes/header.php';
+?>
+
+    <section class="pic"> 
+        <h2>РЧКЧ СВЕЛО</h2>
+        <div class="images">
+           <img src="images/swim.jpg" style="width:100%;" alt="#1"/>
+           <img src="images/nasishort.jpg" style="width:100%;" alt="#1"/>
+        <div>
+    </section>
+    
+<?php
+include 'includes/footer.php';
+?> 

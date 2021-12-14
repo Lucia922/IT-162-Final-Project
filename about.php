3715544d7b003fb2240c4bed8cb941f32d241d75
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-14 03:58:12 -0800

    Added about.php

---
+++ about.php
@@ -0,0 +1,43 @@
+<?php 
+include 'includes/header.php';
+?>
+
+    <section class="pic"> 
+        <h2>РЧКЧ СВЕЛО</h2>
+    </section>
+
+    <article class="about">
+    <h3 class="manifesto"> The Manifesto </h3>
+        <p>
+            Ruku Svelo,<br> roughly translated from Russian,<br> means "Blossoming hand."</br> 
+            My two daughters are the inspiration behind my designs.</br>
+            I want my daughters to lead beautiful lives,</br>
+            to enjoy sophisticated aesthetics,</br>
+            and to be able to navigate emotional complexith.</br>
+                                                             </br>
+            I have been dreaming of children's clothes that are</br>
+            classic, sturdy, comfortable, simple, wholesome, and honest.</br>
+            Ruku Svelo celebrates traditions, the passage of time,</br>
+            and the far-away future.
+        </p>
+    <h3 class="about">About</h3>
+        <p>
+            The birth of Ruku Svelo began<br> with my mother-in-law Ella's wardrobe,<br>
+            which is full of linen dresses, wool stockings,<br> hand-printed cottons,
+            hand-knitted sweaters,<br> and the old, soft, worn textures of Soviet Russia.<br>
+                                                                    <br>
+            As a designer, I worked for ten years in women's apparel.<br>
+            But I quit fast fashion and throw-away culture.<br>
+            Now most of my designs are one-offs,<br> made from rare fabrics,<br>
+            hand-selected for quality,<br>designed to be passed down through several generations.<br>
+            <br> 
+            I live with my husband and two daughters in Maine.<br>
+            We are surrounded by water, mountains,<br>
+            and evergreen forests.                                                        
+        </p>
+    </articla>
+
+    
+<?php
+include 'includes/footer.php';
+?> 

a0bb922766454b46900e3858d2a1b66bb3a6008b
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-13 18:20:03 -0800

    included 'includes' folder with header and footer.php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <p><small>&copy;rukusvelo-<?php echo date("Y")?>/
+           <a href="https://validator.w3.org/check?uri=https%3A%2F%2Frukusvelo.com%2FIT-162-Final-Project%2Findex.php">Check HTML/</a> 
+           <a href="https://jigsaw.w3.org/css-validator/check?uri=referer">Check CSS/ </a></small>
+        </p> 
+        <nav>
+            <ul class="footernav">
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="size.php">Size</a></li>
+                <li><a href="cart.php">Cart</a></li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>IT162 Final Project - Ruku Svelo</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<script src="https://kit.fontawesome.com/e0101396a3.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.php">rukusvelo</a></h1>
+        <nav>
+            <ul class="topnav">
+                <li><a href="editionone.php">#1</a></li>
+                <li><a href="shop.php">Shop</a></li> 
+            </ul>
+        </nav>        
+    </header>

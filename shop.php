541aea496dc731b711b37d5cbfff0241dce29810
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-14 03:59:37 -0800

    Added shop.php

---
+++ shop.php
@@ -0,0 +1,53 @@
+<?php 
+include 'includes/header.php';
+?>
+
+<section class="pic">
+  <h2>РЧКЧ СВЕЛО</h2>
+</section>
+
+<div class="container">
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />    
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+    <div class="item">
+       <p><span>item name / color </br>$000.00</span></p>
+       <img src="images/swim.jpg" alt="swim" />
+    </div>
+</div>
+    
+   
+<?php
+include 'includes/footer.php';
+?> 
+
+

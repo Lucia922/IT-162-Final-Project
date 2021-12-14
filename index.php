e848bbb038fffb461c073d91d46f3ab54cabce11
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-12-13 18:24:24 -0800

    divided header and footer.php and included include code

--- index.php
+++ index.php
@@ -1,44 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>IT162 Final Project - Ruku Svelo</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta charset="utf-8">
-<script src="https://kit.fontawesome.com/e0101396a3.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="css/styles.css" />
-</head>
-
-<body>
-    <header>
-        <h1><a href="index.php">rukusvelo</a></h1>
-        <nav>
-            <ul class="topnav">
-                <li><a href="edition.php">#1</a></li>
-                <li><a href="shop.php">Shop</a></li> 
-            </ul>
-        </nav>        
-    </header>
-
+<?php 
+include 'includes/header.php';
+?>
     <section class="pic">
-        <h2></h2>
-        <img src="images/test.jpg"  style="width:100%;" alt="">
-        
+        <h2>РЧКЧ СВЕЛО</h2>
+        <div class="images">
+           <img src="images/test.jpg"  style="width:100%;" alt=""/>
+        </div>
     </section>
 
-    
-    <footer>
-        <p><small>&copy;rukusvelo-<?php echo date("Y")?>/
-           <a href="https://validator.w3.org/check?uri=referer">Check HTML/</a> 
-           <a href="https://jigsaw.w3.org/css-validator/check?uri=referer">Check CSS/ </a></small>
-        </p> 
-        <nav>
-            <ul class="footernav">
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="size.php">Size</a></li>
-                <li><a href="cart.php">Cart</a></li>
-            </ul>
-        </nav>
-    </footer>
-</body>
-</html>
+<?php
+include 'includes/footer.php';
+?>   
